662401a9b839a25825566bf8d5ad88d2f9b9b05a
zhangyunkai <zhangyunkai@palmax.cn>
2021-01-14 10:30:17 +0800

    + ImgGreyFade FadeController

---
+++ Assets/ImgGreyFade/FadeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+    public RawImage img;
+
+    private void Start()
+    {
+        img.material.SetFloat("_Fade", 0f);
+        Observable.EveryUpdate().Where(_ => Time.time <= 3f).Subscribe(x =>
+        {
+            Debug.Log($"{Time.time}");
+            img.material.SetFloat("_Fade", Time.time/3f);
+        }).AddTo(this);
+    }
+}
