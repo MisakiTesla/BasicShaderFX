4d004c1c02df6748cb1d538e5cb7028ef5f31093
zhangyunkai <zhangyunkai@palmax.cn>
2021-04-15 17:44:31 +0800

    + UIBlur with GaussianBlur

---
+++ Assets/UIBlur/BgBlur.shader
@@ -0,0 +1,90 @@
+﻿Shader "Custom/BgBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Radius ("Radius", float) = 1
+    }
+	
+	CGINCLUDE
+	#include "UnityCG.cginc"
+
+	struct appdata
+	{
+		float4 vertex : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	struct v2f
+	{
+		float2 uv : TEXCOORD0;
+		float4 vertex : SV_POSITION;
+		float4 blurTexcoord: TEXCOORD1;
+	};
+
+	float4 _BlurOffset;
+	uniform float2 _MainTex_TexelSize;
+	uniform float _Radius;
+
+	v2f vert5Horizontal(appdata v)
+	{
+		v2f o;
+		o.vertex = UnityObjectToClipPos(v.vertex);
+		o.uv = v.uv;
+		float2 offset = float2(_MainTex_TexelSize.x * _Radius * 1.33333333, 0.0);
+		o.blurTexcoord = o.uv.xyxy + _BlurOffset.xyxy * float4(1, 1, -1, -1);
+		o.blurTexcoord.xy = v.uv + offset;
+		o.blurTexcoord.zw = v.uv - offset;
+		return o;
+	}
+
+	v2f vert5Vertical(appdata v)
+	{
+		v2f o;
+		o.vertex = UnityObjectToClipPos(v.vertex);
+		o.uv = v.uv;
+		float2 offset = float2(0.0, _MainTex_TexelSize.y * _Radius * 1.33333333);
+		o.blurTexcoord = o.uv.xyxy + _BlurOffset.xyxy * float4(1, 1, -1, -1);
+		o.blurTexcoord.xy = v.uv + offset;
+		o.blurTexcoord.zw = v.uv - offset;
+		return o;
+	}
+
+	sampler2D _MainTex;
+
+	fixed4 frag(v2f i) : SV_Target
+	{
+		//fixed4 col = tex2D(_MainTex, i.uv);
+
+		fixed3 col = tex2D(_MainTex, i.uv).xyz * 0.29411764;
+		col += tex2D(_MainTex, i.blurTexcoord.xy).xyz * 0.35294117;
+		col += tex2D(_MainTex, i.blurTexcoord.zw).xyz * 0.35294117;
+		return fixed4(col, 1.0);
+	}
+
+	ENDCG
+	
+    SubShader
+    {
+    	
+	    Cull Off ZWrite Off ZTest Always
+
+	    Pass
+		{
+			Name "Horizontal" // 0
+			CGPROGRAM
+			#pragma vertex vert5Horizontal
+			#pragma fragment frag
+			ENDCG
+		}
+
+		Pass
+		{
+			Name "Vertical" // 1
+			CGPROGRAM
+			#pragma vertex vert5Vertical
+			#pragma fragment frag
+			ENDCG
+		}
+    }
+}
---
+++ Assets/UIBlur/UIBlur.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBlur : MonoBehaviour
+{
+    public Camera uiCamera;
+    public RawImage rawImage;
+    [Range(0, 4)]
+    public int downSample = 0;
+    [Range(1, 8)]
+    public int iterations = 1;
+    [Range(0f, 1f)]
+    public float interpolation = 1f;
+    public Material blurMaterial;
+    private static readonly int _Radius = Shader.PropertyToID("_Radius");
+    private void OnEnable()
+    {
+        // 创建一个RenderTexture对象  
+        //var rt = new RenderTexture((int)Screen.width>> downSample, (int)Screen.height>> downSample, 0);
+        //尽量用GetTemporary
+        var rt = RenderTexture.GetTemporary(Screen.width>> downSample, Screen.height>> downSample, 24, RenderTextureFormat.Default);
+
+        // 临时设置相关相机的targetTexture为rt, 并手动渲染相关相机  
+        uiCamera.targetTexture = rt;
+        uiCamera.Render();
+        // 重置相关参数，以使用camera继续在屏幕上显示  
+        uiCamera.targetTexture = null;
+        ProcessRT(rt, null);
+        rawImage.texture = rt;
+
+    }
+
+    private void ProcessRT(RenderTexture source, RenderTexture destination)
+    {
+
+            var rt2 = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                // helps to achieve a larger blur
+                //float radius = (float)i * interpolation + interpolation;
+                //blurMaterial.SetFloat(_Radius, radius);
+
+                Graphics.Blit(source, rt2, blurMaterial, 0);
+                source.DiscardContents();
+
+                // is it a last iteration? If so, then blit to destination
+                if (i == iterations - 1)
+                {
+                    Graphics.Blit(rt2, destination, blurMaterial, 1);
+                }
+                else
+                {
+                    Graphics.Blit(rt2, source, blurMaterial, 1);
+                    rt2.DiscardContents();
+                }
+            }
+
+            RenderTexture.ReleaseTemporary(rt2);
+        
+    }
+
+    Texture2D CaptureCamera(Camera camera, Rect rect)   
+    {  
+        // 创建一个RenderTexture对象  
+        RenderTexture rt = new RenderTexture((int)rect.width, (int)rect.height, 0);  
+        // 临时设置相关相机的targetTexture为rt, 并手动渲染相关相机  
+        camera.targetTexture = rt;  
+        camera.Render();  
+        //ps: --- 如果这样加上第二个相机，可以实现只截图某几个指定的相机一起看到的图像。  
+        //ps: camera2.targetTexture = rt;  
+        //ps: camera2.Render();  
+        //ps: -------------------------------------------------------------------  
+
+        // 激活这个rt, 并从中中读取像素。  
+        RenderTexture.active = rt;  
+        Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24,false);  
+        screenShot.ReadPixels(rect, 0, 0);// 注：这个时候，它是从RenderTexture.active中读取像素  
+        screenShot.Apply();  
+
+        // 重置相关参数，以使用camera继续在屏幕上显示  
+        camera.targetTexture = null;  
+        //ps: camera2.targetTexture = null;  
+        RenderTexture.active = null; // JC: added to avoid errors  
+        GameObject.Destroy(rt);  
+        // 最后将这些纹理数据，成一个png图片文件  
+        byte[] bytes = screenShot.EncodeToPNG();  
+        string filename = Application.dataPath + "/Screenshot.png";  
+        System.IO.File.WriteAllBytes(filename, bytes);  
+        Debug.Log(string.Format("截屏了一张照片: {0}", filename));  
+
+        return screenShot;  
+    }
+}
