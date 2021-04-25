8f6946f8304b510a4beed603a521c2b3bfd1e7b5
zhangyunkai <zhangyunkai@palmax.cn>
2021-04-25 16:39:24 +0800

    + SDFTexGenerator

---
+++ Assets/SDFTexGenerator/ImgSDF.shader
@@ -0,0 +1,177 @@
+﻿// Unity built-in shader source. Copyright (c) 2016 Unity Technologies. MIT license (see license.txt)
+
+Shader "Wunder/ImgSDF"
+{
+    Properties
+    {
+        [PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+        _Color ("Tint", Color) = (1,1,1,1)
+
+        _StencilComp ("Stencil Comparison", Float) = 8
+        _Stencil ("Stencil ID", Float) = 0
+        _StencilOp ("Stencil Operation", Float) = 0
+        _StencilWriteMask ("Stencil Write Mask", Float) = 255
+        _StencilReadMask ("Stencil Read Mask", Float) = 255
+
+        _ColorMask ("Color Mask", Float) = 15
+
+        [Toggle(UNITY_UI_ALPHACLIP)] _UseUIAlphaClip ("Use Alpha Clip", Float) = 0
+
+        _SDF ("SDF", Float) = 1
+        _DistanceMark("Distance Mark", float) = .5
+        _OutlineDistanceMark("Outline Distance Mark", float) = .25
+        _GlowDistanceMark("Glow Distance Mark", float) = .25
+        _SmoothDelta("Smooth Delta", float) = .25
+        _ShadowSmoothDelta("Shadow Smooth", float) = .1
+        _GlowSmoothDelta("Glow Smooth", float) = .1
+        _MainColor("Main Color", Color) = (1,1,1,1)
+        _OutlineColor("Outline Color", Color) = (1,0,0,1)
+        _GlowColor("Glow Color", Color) = (1,0,0,1)
+        _ShadowColor("Shadow Color", Color) = (1,0,0,1)
+        _ShadowOffsetX("Shadow Offset X", float) = 0
+        _ShadowOffsetY("Shadow Offset Y", float) = 0
+
+
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "Queue"="Transparent"
+            "IgnoreProjector"="True"
+            "RenderType"="Transparent"
+            "PreviewType"="Plane"
+            "CanUseSpriteAtlas"="True"
+        }
+
+        Stencil
+        {
+            Ref [_Stencil]
+            Comp [_StencilComp]
+            Pass [_StencilOp]
+            ReadMask [_StencilReadMask]
+            WriteMask [_StencilWriteMask]
+        }
+
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        ZTest [unity_GUIZTestMode]
+        Blend SrcAlpha OneMinusSrcAlpha
+        ColorMask [_ColorMask]
+
+        Pass
+        {
+            Name "Default"
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+
+            #include "UnityCG.cginc"
+            #include "UnityUI.cginc"
+
+            #pragma multi_compile __ UNITY_UI_CLIP_RECT
+            #pragma multi_compile __ UNITY_UI_ALPHACLIP
+
+            struct appdata_t
+            {
+                float4 vertex : POSITION;
+                float4 color : COLOR;
+                float2 texcoord : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                fixed4 color : COLOR;
+                float2 texcoord : TEXCOORD0;
+                float4 worldPosition : TEXCOORD1;
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            sampler2D _MainTex;
+            fixed4 _Color;
+            fixed4 _TextureSampleAdd;
+            float4 _ClipRect;
+            float4 _MainTex_ST;
+            float _SDF;
+            //float4 _MainColor;
+            float4 _OutlineColor;
+            float4 _ShadowColor;
+            float4 _GlowColor;
+            float _SmoothDelta;
+            float _ShadowSmoothDelta;
+            float _GlowSmoothDelta;
+            float _DistanceMark;
+            float _OutlineDistanceMark;
+            float _GlowDistanceMark;
+            float _ShadowOffsetX;
+            float _ShadowOffsetY;
+
+            v2f vert(appdata_t v)
+            {
+                v2f OUT;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
+                OUT.worldPosition = v.vertex;
+                OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
+
+                OUT.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+
+                OUT.color = v.color * _Color;
+                return OUT;
+            }
+
+            fixed4 frag(v2f IN) : SV_Target
+            {
+                half4 color = (tex2D(_MainTex, IN.texcoord) + _TextureSampleAdd) * IN.color;
+
+                #ifdef UNITY_UI_CLIP_RECT
+                color.a *= UnityGet2DClipping(IN.worldPosition.xy, _ClipRect);
+                #endif
+
+                #ifdef UNITY_UI_ALPHACLIP
+                clip (color.a - 0.001);
+                #endif
+                float distance = color.a;
+
+                //fixed4 col = tex2D(_MainTex, IN.texcoord);
+
+                fixed4 outlineCol;
+                outlineCol.a = smoothstep(_OutlineDistanceMark - _SmoothDelta, _OutlineDistanceMark + _SmoothDelta,
+                                          distance);
+                outlineCol.rgb = _OutlineColor.rgb;
+
+                fixed4 glowCol;
+                glowCol.a = smoothstep(_GlowDistanceMark - _GlowSmoothDelta, _GlowDistanceMark + _GlowSmoothDelta,
+                                       distance);
+                glowCol.rgb = _GlowColor.rgb;
+
+                float shadowDistance = tex2D(_MainTex, IN.texcoord + half2(_ShadowOffsetX, _ShadowOffsetY));
+                float shadowAlpha = smoothstep(_DistanceMark - _ShadowSmoothDelta, _DistanceMark + _ShadowSmoothDelta,
+                                               shadowDistance);
+                fixed4 shadowCol = fixed4(_ShadowColor.rgb, _ShadowColor.a * shadowAlpha);
+
+                color.rgb = _Color.rgb;
+                color.a = smoothstep(_DistanceMark - _SmoothDelta, _DistanceMark + _SmoothDelta, distance);
+                //if (distance < _DistanceMark)
+                //	col.a = 0.0;
+                //else
+                //	col.a = 1.0;
+                //col.rgb = _MainColor.rgb;
+
+                //return col;
+                return lerp(glowCol, color, color.a);
+                //return lerp(outlineCol, col, col.a);
+                //return lerp(shadowCol, color, color.a);
+
+                //color.a = smoothstep(_SDF, _SDF + 0.25, color.a);
+                return color;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/SDFTexGenerator/SDFTexGenerator.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pixel
+{
+    public bool isIn;
+    public float distance;
+}
+
+public class SDFTexGenerator : MonoBehaviour
+{
+    public Texture2D src;
+    public Texture2D dest;
+    public RawImage rawImageDest;
+    public RawImage rawImageDestWithShader;
+    private readonly string _yourFile = "/UIGlow/computeTex.png"; //路径记得要修改！注意该路径斜线是向左的，和电脑里文件将爱路径相反
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(300, Screen.height - 100, 200, 100), "Generate"))
+        {
+            dest = new Texture2D (64, 64, TextureFormat.Alpha8, false);
+            GenerateSDF(src,dest);
+            rawImageDestWithShader.texture = dest;
+            rawImageDest.texture = dest;
+        }
+        if (GUI.Button(new Rect(300, Screen.height - 200, 200, 100), "Save"))
+        {
+            if (dest)
+            {
+                var savePath = Path.Combine(Application.dataPath + _yourFile);
+                saveTexture2D(dest, savePath);
+            }
+        }
+    }
+
+    public static void GenerateSDF(Texture2D source, Texture2D destination)
+    {
+        int sourceWidth = source.width;
+        int sourceHeight = source.height;
+        int targetWidth = destination.width;
+        int targetHeight = destination.height;
+
+        var pixels = new Pixel[sourceWidth, sourceHeight];
+        var targetPixels = new Pixel[targetWidth, targetHeight];
+        
+        int x, y;
+        Color targetColor = Color.white;
+        for (x = 0; x < sourceWidth; x++)
+        {
+            for (y = 0; y < sourceHeight; y++)
+            {
+                pixels[x, y] = new Pixel();
+                if (source.GetPixel(x, y).a >= 1f)
+                    pixels[x, y].isIn = true;
+                else
+                    pixels[x, y].isIn = false;
+            }
+        }
+
+
+        float gapX = (sourceWidth / (float)targetWidth);
+        float gapY = (sourceHeight / (float)targetHeight);
+        Debug.Log($"{sourceWidth}_{targetWidth}_{gapX},{sourceHeight}_{targetHeight}_{gapY}");
+        
+        int MAX_SEARCH_DIST = 512;
+        int minx, maxx, miny, maxy;
+        float max_distance = -MAX_SEARCH_DIST;
+        float min_distance = MAX_SEARCH_DIST;
+
+        for (x = 0; x < targetWidth; x++)
+        {
+            for (y = 0; y < targetHeight; y++)
+            {
+                targetPixels[x, y] = new Pixel();
+                int sourceX = (int)(x * gapX);
+                int sourceY = (int)(y * gapY);
+                int min = MAX_SEARCH_DIST;
+                minx = sourceX - MAX_SEARCH_DIST;
+                if (minx < 0)
+                {
+                    minx = 0;
+                }
+
+                miny = sourceY - MAX_SEARCH_DIST;
+                if (miny < 0)
+                {
+                    miny = 0;
+                }
+
+                maxx = sourceX + MAX_SEARCH_DIST;
+                if (maxx > (int) sourceWidth)
+                {
+                    maxx = sourceWidth;
+                }
+
+                maxy = sourceY + MAX_SEARCH_DIST;
+                if (maxy > (int) sourceHeight)
+                {
+                    maxy = sourceHeight;
+                }
+
+                int dx, dy, iy, ix, distance;
+                bool sourceIsInside = pixels[sourceX, sourceY].isIn;
+                if (sourceIsInside)
+                {
+                    for (iy = miny; iy < maxy; iy++)
+                    {
+                        dy = iy - sourceY;
+                        dy *= dy;
+                        for (ix = minx; ix < maxx; ix++)
+                        {
+                            bool targetIsInside = pixels[ix, iy].isIn;
+                            if (targetIsInside)
+                            {
+                                continue;
+                            }
+
+                            dx = ix - sourceX;
+                            distance = (int) Mathf.Sqrt(dx * dx + dy);
+                            if (distance < min)
+                            {
+                                min = distance;
+                            }
+                        }
+                    }
+
+                    if (min > max_distance)
+                    {
+                        max_distance = min;
+                    }
+
+                    targetPixels[x, y].distance = min;
+                }
+                else
+                {
+                    for (iy = miny; iy < maxy; iy++)
+                    {
+                        dy = iy - sourceY;
+                        dy *= dy;
+                        for (ix = minx; ix < maxx; ix++)
+                        {
+                            bool targetIsInside = pixels[ix, iy].isIn;
+                            if (!targetIsInside)
+                            {
+                                continue;
+                            }
+
+                            dx = ix - sourceX;
+                            distance = (int) Mathf.Sqrt(dx * dx + dy);
+                            if (distance < min)
+                            {
+                                min = distance;
+                            }
+                        }
+                    }
+
+                    if (-min < min_distance)
+                    {
+                        min_distance = -min;
+                    }
+
+                    targetPixels[x, y].distance = -min;
+                }
+            }
+        }
+
+        //EXPORT texture
+        float clampDist = max_distance - min_distance;
+        for (x = 0; x < targetWidth; x++)
+        {
+            for (y = 0; y < targetHeight; y++)
+            {
+                targetPixels[x, y].distance -= min_distance;
+                float value = targetPixels[x, y].distance / clampDist;
+                destination.SetPixel(x, y, new Color(1, 1, 1, value));
+                destination.Apply(true);
+            }
+        }
+    }
+    
+    public static void saveTexture2D (Texture2D texture, string path) {
+        byte[] bytes = texture.EncodeToPNG ();
+        //UnityEngine.Object.Destroy (texture);
+        System.IO.File.WriteAllBytes (path, bytes);
+        Debug.Log ("write to File over");
+        UnityEditor.AssetDatabase.Refresh (); //自动刷新资源
+    }
+
+}
