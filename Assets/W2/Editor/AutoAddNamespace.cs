c1fbd03dfcc7a9dd4a343005befb2767f98a509b
zhangyunkai <zhangyunkai@palmax.cn>
2021-05-11 18:53:28 +0800

    + AutoAddNamespace

---
+++ Assets/W2/Editor/AutoAddNamespace.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wunder.Editor
+{
+    public class AutoAddNamespace : UnityEditor.AssetModificationProcessor
+    {
+        private static void OnWillCreateAsset(string assetPath)
+        {
+            assetPath = assetPath.Replace(".meta", "");
+            if (assetPath.EndsWith(".cs"))
+            {
+                var text = "";
+                text += File.ReadAllText(assetPath);
+                var className = GetClassName(text);
+                Debug.Log($"{className}");
+
+                var scriptContent = GetNewScriptContent(className);
+                File.WriteAllText(assetPath, scriptContent);
+                AssetDatabase.Refresh();
+            }
+        }
+        
+        /// <summary>
+        /// 新脚本内容
+        /// </summary>
+        private static string GetNewScriptContent(string className)
+        {
+            var scriptBuildHelper = new ScriptBuildHelper();
+            scriptBuildHelper.WriteUsing("UnityEngine");
+            scriptBuildHelper.WriteEmptyLine();
+            scriptBuildHelper.WriteNamespace("Wunder.UI");
+            scriptBuildHelper.IndentTimes++;
+            
+            scriptBuildHelper.WriteClass(className);
+            scriptBuildHelper.IndentTimes++;
+
+            scriptBuildHelper.WriteMethod("Start");
+            return scriptBuildHelper.ToString();
+        }
+        
+
+        private static string GetClassName(string text)
+        {
+            // var datas = text.Split(' ');
+            // var index = 0;
+            // for (int i = 0; i < datas.Length; i++)
+            // {
+            //     if (datas[i].Contains("class"))
+            //     {
+            //         index = i + 1;
+            //         break;
+            //     }
+            // }
+            //
+            // if (datas[index].Contains(":"))
+            // {
+            //     return datas[index].Split(':')[0];
+            // }
+            // else
+            // {
+            //     return datas[index];
+            // }
+            
+            // public class AutoAddNamespace : 
+            var patterm = "public class ([A-Za-z0-9_]+)\\s*:\\s*MonoBehaviour";
+            var match = Regex.Match(text, patterm);
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        
+    }
+}
---
+++ Assets/W2/Editor/ScriptBuildHelper.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public class ScriptBuildHelper
+{
+    private StringBuilder _stringBuilder;
+    private string _lineBreak = "\r\n";
+    private int _currentIndex;
+    public int IndentTimes { get; set; }
+    public ScriptBuildHelper()
+    {
+        _stringBuilder = new StringBuilder();
+    }
+    public void Write(string context, bool isIndent = false)
+    {
+        if (isIndent)
+        {
+            context = GetIndent() + context;
+        }
+
+        if (_currentIndex == _stringBuilder.Length)
+        {
+            _stringBuilder.Append(context);
+        }
+        else
+        {
+            _stringBuilder.Insert(_currentIndex, context);
+        }
+
+        _currentIndex += context.Length;
+    }
+    private void WriteLine(string context, bool isIndent = false)
+    {
+        Write(context + _lineBreak, isIndent);
+    }
+    
+    private string GetIndent()
+    {
+        var indent = "";
+        for (int i = 0; i < IndentTimes; i++)
+        {
+            indent += "    ";
+        }
+        return indent;
+    }
+    
+    private int WriteCurlyBrackets()
+    {
+        var start = _lineBreak + GetIndent() + "{" + _lineBreak;
+        var end = GetIndent() + "}" + _lineBreak;
+        Write(start+end,true);
+        return end.Length; 
+    }
+    
+    public void WriteUsing(params string[] namespaceNames)
+    {
+        foreach (var name in namespaceNames)
+        {
+            WriteLine($"using {name};");
+        }
+    }
+    
+    public void WriteEmptyLine()
+    {
+        Write("");
+    }    
+    public void WriteNamespace(string name)
+    {
+        Write($"namespace {name}");
+        var length = WriteCurlyBrackets();
+        _currentIndex -= length;
+    }
+    
+    public void WriteClass(string name)
+    {
+        Write($"public class {name} : MonoBehaviour",true);
+        var length = WriteCurlyBrackets();
+        _currentIndex -= length;
+
+    }
+
+    public void WriteMethod(string name, params string[] paraNames)
+    {
+        var tmpSB = new StringBuilder();
+        tmpSB.Append($"public void {name}()");
+        if (paraNames.Length > 0)
+        {
+            foreach (var s in paraNames)
+            {
+                tmpSB.Insert(tmpSB.Length - 1, s + ",");
+            }
+
+            tmpSB.Remove(tmpSB.Length - 2, 1);
+            
+        }
+        Write(tmpSB.ToString(), true);
+        WriteCurlyBrackets();
+    }
+
+    public override string ToString()
+    {
+        return _stringBuilder.ToString();
+    }
+}
